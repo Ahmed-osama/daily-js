cd30175fedd1a2244dd2dddabb8ca2a5296a0526
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-15 14:51:43 +0200

    IIFE

--- node/app/app.js
+++ node/app/app.js
@@ -1,2 +1,22 @@
-greet = require('./greet')
-greet()
+
+/*
+#there as global avilable keywords
+    __dirname
+    require
+    exports
+object and object litrels:
+    name value pairs : name maps to value
+    object : a collection of name value pairs
+        sets in memory and points to other values
+    object literal : {name: value, name2:value2, name3:{name4:value4}}
+
+IIFE : immediatly invoked function expression
+scope : where in code you have acess to a virable
+
+*/
+var name = 'john';
+(function(){
+    var name = 'jane';
+    console.log(name)
+}())
+console.log(name)
