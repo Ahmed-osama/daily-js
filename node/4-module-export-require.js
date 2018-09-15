a2180d6adf78a24544b26a9d3bbd584500183f7b
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-15 19:36:55 +0200

    native modules

--- node/4-module-export-require.js
+++ node/4-module-export-require.js
@@ -9,6 +9,32 @@
 
 */
 
+/*
+#there as global avilable keywords
+    __dirname
+    require
+    exports
+object and object litrels:
+    name value pairs : name maps to value
+    object : a collection of name value pairs
+        sets in memory and points to other values
+    object literal : {name: value, name2:value2, name3:{name4:value4}}
+
+IIFE : immediatly invoked function expression
+scope : where in code you have acess to a virable
+json : js object notation , a standered for structuuring data thats inspired by javascript object literals
+mutate : is to change
+revealing module pattern : exposesing only the properties and  methods  you wanted via a returned object
+- in node your code is alwayes wraped inside a dunction
+that passes in 5 prams dirname, file name , require, exports, module
+and it alwayes returns export
+
+- require cashes modules if its already required before
+- global api
+- web server checklist
+    -
+*/
+
 function greet(){
     console.log('hi')
 }
