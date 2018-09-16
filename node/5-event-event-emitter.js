ffde26fbc7747bd2444d0ed1cdca76278f8f9296
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-16 10:21:13 +0200

    object.creat

--- node/5-event-event-emitter.js
+++ node/5-event-event-emitter.js
@@ -6,4 +6,23 @@
     so js side is faking it 
 
     Event listener : a code that responds to an event
+    magic string : string have special meaning in our code
+    Object.creat
+
+    util.inherites : creates shared prototype object between 2 objects
 */
+
+let person = {
+    firstName : '',
+    lastName : '',
+    greet(){
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+
+let john =Object.create(person)
+john.firstName ='john',
+john.lastName ='snow'
+    
+console.log(john.greet())
--- node/app/app.js
+++ node/app/app.js
@@ -1,17 +1,27 @@
 var Emitter = require('events');
 var eventConfig = require('./config').events
-var emtr = new Emitter()
-let person = {
-    name : 'ahmed',
+var emtr = new Emitter();
+var util = require('util');
+
+class Person {
+    constructor(name){
+        this.name = name
+        
+        
+    }
     greet(){
+        this.emit('greet')
         console.log(`good work ${this.name}`)
     }
+    
+
 }
-let person2 = {
-    name : 'osama',
-   
-}
-emtr.on(eventConfig.GREET,()=>console.log('greetings'))
-emtr.on(eventConfig.GREET,()=>console.log('that was agreet'))
-emtr.on(eventConfig.GREET,person.greet.bind(person2))
-emtr.emit(eventConfig.GREET)
+let person =  new Person('ahmed')
+let person2 = new Person('osama')
+
+util.inherits(Person,Emitter)
+// emtr.on(eventConfig.GREET,()=>console.log('greetings'))
+// emtr.on(eventConfig.GREET,()=>console.log('that was agreet'))
+// emtr.on(eventConfig.GREET,person.greet.bind(person2))
+// emtr.emit(eventConfig.GREET)
+person2.emit(eventConfig.GREET)
