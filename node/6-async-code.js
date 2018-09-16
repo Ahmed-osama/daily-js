f4621ca791b462a77ccc388adf8e0328dcf14c42
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-16 21:29:53 +0200

    streams & buffers

--- node/6-async-code.js
+++ node/6-async-code.js
@@ -6,6 +6,12 @@
     call back : a function we pass to other function which it will be assumed to be invoked inside the main function
 
     none blocking : doing other things w/o stopping your programme from running
+
+    buffer : a temperoray holding spot for adata being moved from on place to another 
+        gather some data and move it along [hel] l o
+
+    stream : a sequnce of data made avilabel over time
+    
     -------------------------
 
     systemEvents : c++ core -> libuv : deals with things happning in lower level
