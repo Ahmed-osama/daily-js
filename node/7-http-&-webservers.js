16005166ee8784bd9140e7b229353f96312fb9ec
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-19 00:51:03 +0200

    http

--- node/7-http-&-webservers.js
+++ node/7-http-&-webservers.js
@@ -6,7 +6,11 @@
     port : once a computer recives a packet , how it knows what program to send it to
         port is just a number the computer program is listening to that port
 
-    
+    HTTP : aset of Rules (and a format) for data being transfered on the web
+        stands for Hyper Text Transfer Protocol
+    mime type : a standered for specifing the type of data being sent,
+        stands for mult purpose internet mail extention ex:
+        [application/json, text/html, image/jpeg]
     
     -------------------------
     browser & server  both have ip and opens socket
