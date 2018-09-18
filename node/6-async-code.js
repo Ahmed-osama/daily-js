8e37f158e5fcdb88b286e95e832e7f14a28c4829
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-18 15:21:40 +0200

    binary data , charchter sets, encoding

--- node/6-async-code.js
+++ node/6-async-code.js
@@ -11,7 +11,20 @@
         gather some data and move it along [hel] l o
 
     stream : a sequnce of data made avilabel over time
+
+    binary data : data stored in binary sets of 1s and 0s
+        each one or zero is called a bit or binary digit
     
+    character set : a representation of characters as numbers
+        each char gets a number , unicode and ascii are character sets.
+
+        h   e   l   l   o
+        |   |   |   |   |       [in unicode]
+       104 101 108 108 111
+                                utf-8
+        has 8 digits to represent the number in binary
+    character encoding : how character are sotred in binary
+
     -------------------------
 
     systemEvents : c++ core -> libuv : deals with things happning in lower level
