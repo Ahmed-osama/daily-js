9e017320e0af03f960d91403d10fbb062e42910a
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-19 00:41:38 +0200

    address / port

--- node/7-http-&-webservers.js
+++ node/7-http-&-webservers.js
@@ -1,43 +1,16 @@
 /*
-    asynchronous: more than one process running simultaneously
-
-    synchronous : one process executing at a time
-
-    call back : a function we pass to other function which it will be assumed to be invoked inside the main function
-
-    none blocking : doing other things w/o stopping your programme from running
-
-    buffer : a temperoray holding spot for adata being moved from on place to another 
-        gather some data and move it along [hel] l o
-
-    stream : a sequnce of data made avilabel over time
-
-    binary data : data stored in binary sets of 1s and 0s
-        each one or zero is called a bit or binary digit
+    protocol : a set of  reals 2 sides a gree to use when comunicating
+    tcp : transimition control protcol, splits data to  pices and transfer from
+        computer to another  as packets
     
-    character set : a representation of characters as numbers
-        each char gets a number , unicode and ascii are character sets.
+    port : once a computer recives a packet , how it knows what program to send it to
+        port is just a number the computer program is listening to that port
 
-        h   e   l   l   o
-        |   |   |   |   |       [in unicode]
-       104 101 108 108 111
-                                utf-8
-        has 8 digits to represent the number in binary
-    character encoding : how character are sotred in binary
-    byte : 8 bits 10101100
-
-    es6 typed arrays : 
-    callback : a function passed to other function which we assume will be invoked at some point
-    STREAM : ASEQUNCE OF PICES OF DATA
-    CHUNK : A PICE OF DATA BEING SENT THROUGH STRAME
-    abstract (base) class : a type of constructor you never work directly with but inherit from
-    Pipe : conection 2 streams by writing to one stream what is being read from another
-    method chaining : that happens when a method of object returns an object
+    
+    
     -------------------------
-
-    systemEvents : c++ core -> libuv : deals with things happning in lower level
-    libuv : has a queue [ , , , ,] and the event loop
-    err 1st callback : a call back that take an error as the first parameter
+    browser & server  both have ip and opens socket
+        http:for web
+        FTP : for files
+        STMP : for emails
 */
-// var buff = new Buffer('hello')
-// console.log(buff.toJSON())
