dbc30674f44e9f68b5ecbfb13692fa2b65e3ad33
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-20 00:02:12 +0200

    respond with html or json

--- node/app/app.js
+++ node/app/app.js
@@ -1,8 +1,10 @@
 
 let http  = require('http');
+let fs = require('fs');
 http.createServer(function(req, res){
     res.writeHead(200, {
-        'content-type':'text/plain'
+        'content-type':'text/json'
     })
-    res.end('Hello my 1st server')
+    let jsonFile = fs.readFileSync(__dirname + '/data.json')
+    res.end(jsonFile)
 }).listen(1337,'127.0.0.1')
