b563e2d77a58561fe4f30ed17139906f460e0524
Ahmed-osama <ahmed.osama@tajawal.com>
2018-09-25 11:55:57 +0200

    Mean

---
+++ node/10-mean.js
@@ -0,0 +1,20 @@
+/*
+    - npm i express --save
+    env : global vilabels specific to the enviroment (server) our code is living in.
+
+    HTTP METHOD : specifies the type of action the request wishes to make
+        ex : [get, post, delet].. also called verbs.
+    
+    middle ware : is code that sits between to layers of software
+
+    static files : the files that wont be processed like [css, img, fonts]
+
+    rest : an architectural style for building apis
+        stands for 'representational state transfer' we decide that HTTP verbs
+        and URL meands something
+        - follow good url structure
+        - deal with http verbs
+
+    x   x
+
+*/
