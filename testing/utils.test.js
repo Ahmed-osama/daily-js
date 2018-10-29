d38272ff52e52b239f9a3c91af58dd14a1d20b93
Ahmed-osama <ahmed.osama@tajawal.com>
2018-10-29 15:57:25 +0200

    jest

--- testing/utils.test.js
+++ testing/utils.test.js
@@ -24,3 +24,29 @@ test('SHould be the same ', () => {
 test('SHould be the same ', () => {
   expect(utils.checkValue(' ')).toBeTruthy();
 });
+
+// toBe is for permitive types
+
+test('user should be ahmed osama', () => {
+  expect(utils.creatUser()).toEqual({
+    firstName: 'Ahmed',
+    lastName: 'Osama',
+  });
+});
+
+test('should be under 1600', () => {
+  const load1 = 600;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThanOrEqual(1500);
+});
+test('should be under 1600', () => {
+  const load1 = 600;
+  const load2 = 700;
+  expect(load1 + load2).toBeGreaterThan(1000);
+});
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/i/);
+});
+teamtest('there is no I in team', () => {
+  expect('team').toMatch(/team/);
+});
