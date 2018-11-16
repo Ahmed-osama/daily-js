43c7dc0a6c9afdb8f76d9cda7704259fd8a240d0
Ahmed-osama <ahmed.osama@tajawal.com>
2018-11-16 11:24:20 +0200

    canvas basics

---
+++ canvas/canvas.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+ctx.fillRect(100, 100, 100, 100)
+;
