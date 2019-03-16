86f618abb914a05c704e281e8d28f512fb02ddab
Ahmed-osama <ahmed.osama@tajawal.com>
2019-03-16 23:01:50 +0200

    local variabels

--- angular/up-and-running/src/app/user/user.component.ts
+++ angular/up-and-running/src/app/user/user.component.ts
@@ -4,15 +4,22 @@ import { EventEmitter } from "@angular/core";
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
-  styleUrls: ["./user.component.scss"]
+  styles: [
+    `
+      .red {
+        color: red;
+      }
+    `
+  ]
 })
 export class UserComponent implements OnInit {
   constructor() {}
-
+  innerAge = 90;
   ngOnInit() {}
   handleClick() {
     this.showAge.emit();
   }
+  log = console.log;
   @Input() user: any;
   @Output() showAge = new EventEmitter();
 }
