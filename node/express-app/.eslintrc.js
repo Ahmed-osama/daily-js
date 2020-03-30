d7b3a7454478002d827b947bf5e8825bf6291bea
Ahmed-osama <ahmed.osama@tajawal.com>
2020-03-30 23:47:56 +0400

    implemented prettier and eslint

---
+++ node/express-app/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true
+    },
+    'extends': ['airbnb-base', 'prettier'],
+    'plugins': ['prettier'],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
