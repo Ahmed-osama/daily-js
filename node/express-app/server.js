ae71ac6896ec7c4eb4be77fc1aaca8839c5ec45c
Ahmed-osama <ahmed.osama@tajawal.com>
2020-03-31 00:45:27 +0400

    create roote

--- node/express-app/server.js
+++ node/express-app/server.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const path = require('path');
-
+const routes = require('./routes/index')
 const app = express();
 const PORT = 300;
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, './static')));
+app.use('/', routes())
 app.set('views', path.join(__dirname, './views'));
 
-app.get('/', (req, res) => {
-  res.render('pages/index', {
-    pageTitle: 'Ahmed Osama'
-  });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`horray server started on port http://localhost:${PORT}`);
 });
+
