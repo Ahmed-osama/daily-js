82456558d89bc0395f499eb4c67aa31e759d9738
Ahmed-osama <ahmed.osama@tajawal.com>
2020-03-30 23:28:22 +0400

    express serve html

--- node/express-app/server.js
+++ node/express-app/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 300;
 
+app.use(express.static(path.join(__dirname, './static/')));
+
 app.get('/', (req, res) => {
-  res.send('hello express here :D');
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(PORT, () => {
