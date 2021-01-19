3428000778d5152af2a33bbb8551bcb5c2e28c53
Ahmed-osama <ahmod.osama@gmail.com>
2021-01-19 23:12:25 +0400

    dapp

--- web-technologies/pwa/sw.js
+++ web-technologies/pwa/sw.js
@@ -1,8 +1,9 @@
 const preCacheList = [
   "/",
-  "/assets",
   "/assets/1_oOcY2Gn-LVt1h-e9xOv5oA.jpeg",
   "/assets/main.js",
+  "/assets/style.css",
+  "https://api.github.com/users?per_page=100&page=1",
 ];
 
 self.addEventListener("install", (event) => {
@@ -11,4 +12,22 @@ self.addEventListener("install", (event) => {
       cache.addAll(preCacheList);
     })
   );
+});
+
+addEventListener("fetch", (event) => {
+  // Prevent the default, and handle the request ourselves.
+  try {
+    event.respondWith(
+      (async function () {
+        // Try to get the response from a cache.
+        const cachedResponse = await caches.match(event.request);
+        // Return it if we found one.
+        if (cachedResponse) return cachedResponse;
+        // If we didn't find a match in the cache, use the network.
+        return fetch(event.request);
+      })()
+    );
+  } catch (err) {
+    return fetch(event.request);
+  }
 });
