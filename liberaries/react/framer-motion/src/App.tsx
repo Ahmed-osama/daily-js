83fecbdd01b0003e1c8fa86578b3d70e6d5145cf
ahmed osama <aosama@adres.ae>
2022-02-18 17:30:49 +0400

    state based animation

--- liberaries/react/framer-motion/src/App.tsx
+++ liberaries/react/framer-motion/src/App.tsx
@@ -7,12 +7,37 @@ import { useState } from "react";
 
 const AppWrapper = styled(motion.main)`
   background: black;
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+`;
+const Ball = styled(motion.main)`
+  background: white;
+  height: 5vw;
+  width: 5vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  border-radius: 50%;
 `;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-  return <AppWrapper className="App"></AppWrapper>;
+  return (
+    <AppWrapper className="App">
+      <Ball
+        onClick={() => setIsActive(!isActive)}
+        animate={{
+          scale: [1, 1.5, 1],
+          x: isActive ? -100 : 100,
+        }}
+      />
+    </AppWrapper>
+  );
 }
 
 export default App;
